5a0b424da7f20894c3c963c391682fc4baf54fc4
anitkumarsingh <anikumarsingh99@gmail.com>
2023-02-12 15:45:14 +0530

    refactor

--- src/pages/Color/ColorGuess.js
+++ src/pages/Color/ColorGuess.js
@@ -1,56 +1,63 @@
 import React, { useState, useEffect } from 'react';
 
+const digits = [
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F'
+];
+const generateRandomColor = () => {
+	const color = new Array(6)
+		.fill('')
+		.map(() => digits[Math.floor(Math.random() * digits.length)])
+		.join('');
+	return `#${color}`;
+};
+
 const ColorGuess = () => {
 	const [currentColor, setCurrentColor] = useState('');
-	const [optionOne, setOptionOne] = useState(null);
-	const [optionTwo, setOptionTwo] = useState(null);
+	const [answers, setAnswers] = useState([]);
 	const [msg, setMsg] = useState('');
 
-	const digits = [
-		'0',
-		'1',
-		'2',
-		'3',
-		'4',
-		'5',
-		'6',
-		'7',
-		'8',
-		'9',
-		'A',
-		'B',
-		'C',
-		'D',
-		'E',
-		'F'
-	];
-	const generateRandomColor = () => {
-		const color = new Array(6)
-			.fill(1)
-			.map((i) => digits[Math.floor(Math.random(digits) * digits.length + 1)])
-			.join('');
-		return `#${color}`;
-	};
-
 	useEffect(() => {
-		setCurrentColor(generateRandomColor());
-		setOptionOne(generateRandomColor());
-		setOptionTwo(generateRandomColor());
-	}, [msg]);
+		const actualAns = generateRandomColor();
+		setCurrentColor(actualAns);
+		let options = [generateRandomColor(), generateRandomColor(), actualAns].sort((a, b) =>
+			a > b ? 1 : -1
+		);
+		setAnswers(options);
+	}, []);
 
 	const checkForAnwser = (opt) => {
+		console.log('curr', currentColor, opt);
 		if (currentColor === opt) {
 			setMsg('Correct Answer');
+			setCurrentColor(generateRandomColor());
 		} else setMsg('Wrong Answer');
 	};
-	console.log('asdasasdasa', msg);
+
 	return (
 		<>
 			<div className='color-guess' style={{ backgroundColor: currentColor }}></div>
 			<br />
-			<button onClick={() => checkForAnwser(`${currentColor}`)}>{currentColor}</button>
-			<button onClick={() => checkForAnwser(`${optionTwo}`)}>{optionTwo}</button>
-			<button onClick={() => checkForAnwser(`${optionOne}`)}>{optionOne}</button>
+			{answers.map((ans) => (
+				<button key={ans} onClick={() => checkForAnwser(`${ans}`)}>
+					{ans}
+				</button>
+			))}
+
 			{msg === 'Correct Answer' ? <p style={{ color: 'green' }}>Correct Answer</p> : null}
 			{msg === 'Wrong Answer' ? <p style={{ color: 'red' }}>Wrong Answer</p> : null}
 		</>
